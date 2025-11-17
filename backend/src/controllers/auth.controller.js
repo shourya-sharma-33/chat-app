9f2c4f17fd36a93705358ceebbab283d8369f557
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-11-17 15:46:36 +0530

    signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,31 @@
-export const signup = (req, res) => {
-    res.send("Signup Route");
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt"
+import { generateToken } from "../lib/utils.js";
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+        if (!password || password.length < 6) {
+            return res.status(400).json({
+                message: "password must be at least 6 characters"
+            });
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+        const newUser = await User.create({ fullName : fullName, email : email, password : hashPassword });
+        if (newUser) {
+            generateToken(newUser._id, res)
+            await newUser.save();
+        }    
+        return res.status(200).json(newUser);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error" });
+    }
 };
 
 export const login = (req, res) => {
