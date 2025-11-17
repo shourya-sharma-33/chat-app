97e32f8d2c467ea7de2726869bfd32f15f18a418
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-11-17 14:24:16 +0530

    routes

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,33 @@
+// IMPORT
 import express from "express";
-import env from 'dotenv';
+import authRoutes from "./src/routes/auth.route.js";
+// END - IMPORT
 
-function appRunCallback () {
-    console.log(`Server Start on ${process.env.BACKEND_PORT}`);
+// SERVER RUN CALLBACK
+function appRunCallback() {
+    console.log(`Server Start on port 5000`);
 }
+// END - SERVER RUN CALLBACK
 
-
-function main(){
+// MAIN
+function main() {
+    // EXPRESS INIT
     const app = express();
+    // END - EXPRESS INIT
+
+    // MIDDLEWARE
+    app.use(express.json());
+
+    // ROUTES
+    app.use("/api/auth", authRoutes); // ADD leading slash
+    // END - ROUTES
+
+    // LISTENING ON PORT
     app.listen(5000, () => {
         appRunCallback();
-    })
+    });
+    // END - LISTENING ON PORT
 }
+// END - MAIN
 
 main();
