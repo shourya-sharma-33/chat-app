ea0a769678e77bbb173e6a89f42ce97c8b0b56e1
shourya-sharma-33 <shourya-ml-dl-artificial-intelligence@proton.me>
2025-11-17 14:34:36 +0530

    controlle

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,18 +1,13 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.send("signup route");
-});
+router.get("/signup", signup);
 
-router.get("/login", (req, res) => {
-    res.send("login route");
-});
+router.get("/login", login);
 
-router.get("/logout", (req, res) => {
-    res.send("logout route");
-});
+router.get("/logout",logout);
 
 export default router;
 
